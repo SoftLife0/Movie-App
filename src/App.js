fd10e1845e6d3e0dad2eba4c035fc2c7ccb9fa16
SoftLife0 <onikosiadewale18@gmail.com>
2024-03-28 18:45:44 +0000

    fetching movie using OMDB api

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import './App.css'
 import SearchIcon from './search.svg'
+import MovieCard from './MovieCard';
 
 const apiUrl = 'http://www.omdbapi.com/?i=tt3896198&apikey=6075ddc'
 const App = () => {
@@ -8,20 +9,40 @@ const App = () => {
     const searchMovie = async (title) => {
         const response = await fetch(`${apiUrl}&s=${title}`);
         const data = await response.json();
-        console.log(data.Search);
+        setMovies(data.Search);
     };
 
     useEffect(() => {
         searchMovie('Shrek');
     }, []);
 
+    const [movies, setMovies] = useState([]);
+
     return(
         <div className='app'>
             <h1>Softlife MovieLand</h1>
-            
+
             <div className='search'>
-                <input type="text" />
+                <input type="text" placeholder='Search for movies' value='Superman' onChange={() => {}}/>
+                <img src={SearchIcon} onClick={() => {}}/>
             </div>
+
+            {
+                movies?.length > 0 
+                ? (
+                    <div className='container'>
+                        {movies.map((movie) => (
+                            <MovieCard movie={movie}/>
+                        ))}
+                    </div>
+                ) : (
+                    <div className='empty'>
+                        <h2>No movie found</h2>
+                    </div>
+                )
+            }
+
+            
         </div>
 
     );
